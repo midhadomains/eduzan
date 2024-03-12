3f05aa7fd938b70fc19779d6ea77b0945bd5be48
rajeshyadav9931 <rajeshkumaryadav9931grd@gmail.com>
2024-03-12 14:05:28 +0530

    platform section animation added

--- app/page.js
+++ app/page.js
@@ -5,12 +5,18 @@ import Image from 'next/image'
 import Accordion from "../components/Accordian";
 import HomeCarousel from "../components/ReviewCarousel";
 import Whatsapp from "../components/Whatsapp";
+import Platform from "../components/Platform";
+
+
 export const metadata = {
     title: "Welcome to MidhFin",
     description: "MidhaFin Home , Explore our Wide Range of Courses "
 }
 
 export default function Home() {
+
+    
+      
     return (
         <>
             <div className="min-h-screen  relative">
@@ -92,55 +98,7 @@ export default function Home() {
                         </div>
                     </div>
                     {/* Our Platform Features */}
-                    <div className='bg-[#FFF5EE] mt-8 m-1 pb-5 text-center'>
-                        <div className='max-w-[90%] sm:max-w-[70%] lg:max-w-[50%] mx-auto mb-5 text-center my-3'>
-                            <h1 className='text-[40px] md:text-[50px] font-bold my-3'>Our <span className='text-[#6A1C1A]'>Platform</span> Features</h1>
-                            <p className='my-4'>We at MidhaFin, focus on striking a careful balance, between Knowledge and Relevance, between theory and analytics & between pure concepts and practical application.</p>
-                        </div>
-                        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-[80%] mx-auto gap-[50px] item-center'>
-                            <div className='mx-auto items-center flex flex-col justify-around'>
-                                <Image width={150} height={100} className='mx-auto  m-2' src="/Study-planner.png" alt="Study Planner" />
-                                <h1 className='text-[20px] font-bold my-3'>Study Planner</h1>
-                                <Link href='/study-planner'><button className='w-full bg-[#6A1C1A] text-white py-2 px-[10px]  sm:px-[30px] lg:px-[40px] xl:px-[50px] rounded  hover:bg-black'>READ MORE</button></Link>
-                            </div>
-                            <div className='mx-auto items-center flex flex-col justify-around'>
-                                <Image width={150} height={100} className='mx-5 m-1' src='/question-bank.png' alt="Questions Bank" />
-                                <h1 className='text-[20px] font-bold my-3'>Questions Bank</h1>
-                                <Link href='/question-bank'><button className='w-full bg-[#6A1C1A] text-white py-2 px-[10px] sm:px-[30px] lg:px-[40px] xl:px-[50px]] rounded  hover:bg-black'>READ MORE</button></Link>
-                            </div>
-                            <div className='mx-auto items-center flex flex-col justify-around'>
-                                <Image width={150} height={100} className='mx-auto m-2' src="/mock-test.png" alt="Mock Tests" />
-                                <h1 className='text-[20px] font-bold my-3'>Mock Tests</h1>
-                                <Link href='/mock-test'><button className='w-full bg-[#6A1C1A] text-white py-2 px-[10px] sm:px-[30px] lg:px-[40px] xl:px-[50px] rounded  hover:bg-black'>READ MORE</button></Link>
-                            </div>
-                            <div className='mx-auto items-center flex flex-col justify-around'>
-                                <Image width={150} height={100} className='mx-auto m-2' src="/lecture-video.png" alt="Lecture Video" />
-                                <h1 className='text-[20px] font-bold text-center my-3'>Lecture Video</h1>
-                                <Link href='/lecture-video'><button className='w-full bg-[#6A1C1A] text-white py-2 px-[10px] sm:px-[30px] lg:px-[40px] xl:px-[50px] rounded  hover:bg-black'>READ MORE</button></Link>
-                            </div>
-                            <div className='mx-auto items-center flex flex-col justify-around'>
-                                <Image width={150} height={100} className='mx-auto m-1' src="/doubt-solving-forum.png" alt="Doubt Solving Forum" />
-                                <h1 className='text-[20px] my-3 font-bold'>Doubt Solving Forum</h1>
-                                <Link href='/doubt-solving-forum'><button className='w-full mx-auto bg-[#6A1C1A] text-white py-2 px-[10px] sm:px-[30px] lg:px-[40px] xl:px-[50px] rounded  hover:bg-black'>READ MORE</button></Link>
-                            </div>
-                            <div className='mx-auto items-center flex flex-col justify-around'>
-                                <Image width={150} height={100} className='mx-auto m-4' src="/industry-expert-faculty.png" alt="Industry Expert Faculty" />
-                                <h1 className='text-[20px] my-3 font-bold'>Industry Expert Faculty</h1>
-                                <Link href='/industry-expert-faculty'><button className='w-full bg-[#6A1C1A] text-white py-2 px-[10px] sm:px-[30px] lg:px-[40px] xl:px-[50px]] rounded  hover:bg-black'>READ MORE</button></Link>
-                            </div>
-                            <div className='mx-auto items-center flex flex-col justify-around '>
-                                <Image width={150} height={100} className='mx-auto m-2' src="/Complete-study-material.png" alt="Complete Study Material" />
-                                <h1 className='text-[20px] my-3 font-bold text-center'>Complete Study Material</h1>
-                                <Link href='/complete-study-material'><button className='w-full bg-[#6A1C1A] text-white py-2 px-[10px] sm:px-[30px] lg:px-[40px] xl:px-[50px] rounded  hover:bg-black'>READ MORE</button></Link>
-                            </div>
-                            <div className='mx-auto items-center flex flex-col justify-around'>
-                                <Image width={150} height={100} className='mx-auto m-4' src="/access-till-you-pass.png" alt="Access Till You P150" />
-                                <h1 className='text-[20px] my-3 font-bold'>Access Till You Pass&nbsp;&nbsp;</h1>
-                                <Link href='/access-till-you-pass'><button className='w-full bg-[#6A1C1A] text-white py-2 px-[10px] sm:px-[30px] lg:px-[40px] xl:px-[50px] rounded hover:bg-black'>READ MORE</button></Link>
-                            </div>
-
-                        </div>
-                    </div>
+                    <Platform/>
                     {/* FAQs  Section */}
                     <div className='max-w-[100%] mx-auto  mt-5 bg-white' >
                         <div>
