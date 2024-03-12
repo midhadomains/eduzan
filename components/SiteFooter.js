26a594bd0a0b251d9c8ae50a4e8bf54794ebb5db
sujalsahu620 <sahusujal620@gmail.com>
2024-03-12 18:01:29 +0530

    done

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -9,7 +9,7 @@ import { TiMail } from "react-icons/ti";
 
 function Footer() {
     return (
-        <footer className="bg-gray-900">
+        <footer className="bg-gray-900 mt-5">
             <div className="container px-6 py-12 mx-auto">
                 <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
                     {/* <div className="sm:col-span-2">
