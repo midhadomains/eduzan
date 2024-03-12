8f99e11cb961c7e1534301e0d5d91bc719c3aa8d
midhadomains <159537481+midhadomains@users.noreply.github.com>
2024-03-12 18:08:16 +0530

    Update HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -19,7 +19,7 @@ function HeroSection() {
                 <div className='flex flex-col item-center m-5 justify-around sm:w-[60%] lg:w-[45%] w-[100%]  mx-auto '>
                     {/* add {typeEffect} instead  of Finance  and add text left*/}
                     <h1 className=' text-[27px] sm:text-[28px] text-center md:text-[30px] lg:text-[40px] xl:text-[50px] font-bold shadow-[rgba(0, 0, 0, 0.24) 0px 3px 8px;'><span className='text-[#6A1C1A]'>Boost your career </span> in  <span className='text-[#6A1C1A]'>Finance</span>  with  mentorship you can bank on <span className='text-[#6A1C1A]'>!</span></h1>
-                    <p className=' w-[100%] lg:w-[95%] text-left xl:w-[70%] text-[#391601] text-[1rem] sm:text-lg  lg:text-xl mx-auto '>Step up your career by acquiring the desirable skills and leading global certifications in Finance.</p>
+                    <p className=' w-[100%] lg:w-[95%]  xl:w-[70%] text-[#391601] text-[1rem] sm:text-lg  lg:text-xl mx-auto '>Step up your career by acquiring the desirable skills and leading global certifications in Finance.</p>
                     <div className='flex mt-5 justify-center'>
                         <Link href="https://midhafin.ongraphy.com/s/authenticate" >
                             <button className='text-sm sm:text-md lg:text-xl bg-[#6A1C1A] rounded text-white hover:bg-black py-3 px-2 sm:px-4 md:px-3 lg:px-4 m-2 ease-in duration-300  '>SIGNUP NOW</button>
