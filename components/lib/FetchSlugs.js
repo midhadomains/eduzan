fb490d0b642d0fd739f0f674f058a693dcf6b3fd
Sujal Sahu <sahusujal620@gmail.com>
2024-05-24 17:16:00 +0530

    done

--- components/lib/FetchSlugs.js
+++ components/lib/FetchSlugs.js
@@ -2,18 +2,22 @@ import { fetchGraphQLData } from './graphqlRequest';
 
 const SLUGS_QUERY = `
   query {
-    allSlugs {
-      slug
+    blogs {
+      edges {
+        node {
+          slug
+        }
+      }
     }
   }
 `;
 
 export const fetchSlugs = async () => {
-    try {
-        const result = await fetchGraphQLData(SLUGS_QUERY);
-        return result.allSlugs.map((item) => item.slug);
-    } catch (error) {
-        console.error('Error fetching slugs:', error);
-        return [];
-    }
+  try {
+    const result = await fetchGraphQLData(SLUGS_QUERY);
+    return result.blogs.edges.map(edge => edge.node.slug);
+  } catch (error) {
+    console.error('Error fetching slugs:', error);
+    return [];
+  }
 };
