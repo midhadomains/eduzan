47190325b62568333ed5285a93dfede586764660
Sujal Sahu <sahusujal620@gmail.com>
2024-06-10 12:48:51 +0530

    done

---
+++ next-sitemap.config.js
@@ -0,0 +1,34 @@
+const { fetchBlogSlugs } = require('./fetchBlogSlugs');
+
+/** @type {import('next-sitemap').IConfig} */
+const config = async () => {
+    const slugs = await fetchBlogSlugs();
+    const blogUrls = slugs.map(slug => `/blog/${slug}`);
+
+    return {
+        siteUrl: 'https://www.midhafin.com', // Replace with your site's URL
+        generateRobotsTxt: true, // Generate a robots.txt file
+        changefreq: 'weekly',
+        priority: 0.7,
+        sitemapSize: 5000,
+        exclude: ['/thank-you'], // Exclude specific pages
+        robotsTxtOptions: {
+            policies: [
+                {
+                    userAgent: '*',
+                    allow: '/',
+                    disallow: ['/thank-you'], // Exclude specific pages from crawling
+                },
+            ],
+        },
+        additionalPaths: async (config) => {
+            return blogUrls.map(url => ({
+                loc: url,
+                changefreq: 'weekly',
+                priority: 0.7,
+            }));
+        },
+    };
+};
+
+module.exports = config;
