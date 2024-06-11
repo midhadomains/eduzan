fd823cef21891f026dc20185433e49f1d2382870
Sachin <sachinsemwal18@gmail.com>
2024-06-11 16:55:15 +0530

    Added Question Banks page

--- app/question-banks/page.js
+++ app/question-banks/page.js
@@ -1,63 +1,109 @@
-import React from 'react';
-import Image from 'next/image'
-import Footer from '../../components/common/SiteFooter';
-import Navbar from '../../components/common/SiteHeader';
+import React from "react";
+import Image from "next/image";
+import Footer from "../../components/common/SiteFooter";
+import Navbar from "../../components/common/SiteHeader";
+import Link from "next/link";
 
 export const metadata = {
-    title: "Question Banks",
-  };
-
+  title: "Question Banks",
+};
 
 const MyComponents3 = () => {
-    return (
-        <div className='select-none'>
-            <Navbar />
-            <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-                <div className="absolute inset-0 -z-10 overflow-hidden">
-                    <svg className="absolute left-[max(50%,25rem)] top-0 h-[94rem] w-[128rem] -translate-x-1/2 stroke-gray-200" aria-hidden="true">
-                        <defs>
-                            <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-                                <path d="M100 200V.5M.5 .5H200" fill="none" />
-                            </pattern>
-                        </defs>
-                        <svg x="50%" y="-1" className="overflow-visible fill-gray-50">
-                            <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" strokeWidth="0" />
-                        </svg>
-                        <rect width="100%" height="100%" strokeWidth="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
-                    </svg>
-                </div>
-                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
-                    <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-                        <div className="lg:pr-4">
-                            <div className="lg:max-w-lg">
-                                <p className="text-2xl pl-1 font-semibold leading-7 text-[#842020]">Features</p>
-                                <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl"> Question <span style={{ color: '#842021' }}>Banks</span></h1>
-                                <p className="mt-12  text-[1rem] lg:text-xl sm:text-[1rem] md:text-lg leading-6 text-gray-700 tracking-wide">Our team has prepared thousands of FRM®/CFA/PRM practice questions which would allow students to test their knowledge. These questions are updated based on the current FRM®/CFA/PRM trends and are a good mixture of easy, medium, and difficult questions. An easy to comprehend solution for every single question is provided along with the questions.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="-ml-4 sm:ml-0 lg:p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-
-                        <Image
-                            src="/question-bank.svg"
-                            alt="question-bank"
-                            className="w-[15rem] max-w-none rounded-xl shadow-l ring-1 ring-gray-400/10 sm:w-[30rem] mx-auto"
-                            width={300}
-                            height={300}
-                        />
-                    </div>
-                    <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-                        <div className="lg:pr-4">
-                            <div className="max-w-auto text-xl leading-7 text-gray-700 lg:max-w-lg tracking-wide">
-                                <p className='mt-8 text-[1rem] lg:text-xl sm:text-[1rem] md:text-lg leading-6'>Our questions are mapped with the FRM®/CFA/PRM exam&apos;s latest curriculum.</p>
-                                <p className="mt-8  text-[1rem] lg:text-xl sm:text-[1rem] md:text-lg leading-6">Further, the organization structure of the FRM®/CFA/PRM questions is extremely user-friendly. The questions are sequenced according to our student study plan.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="select-none">
+      <Navbar />
+      <div className="max-w-[1300px] mx-auto py-4 lg:py-20 px-[20px] lg:px-[40px]">
+        <div className=" ">
+          <p className="text-[16px] sm:text-[24px] pl-1 font-semibold leading-7 text-center mg:text-left text-[#842020]">
+            Features
+          </p>
+          <h1 className="text-4xl font-bold tracking-tight text-center mg:text-left text-gray-900 text-[24px] md:text-[32px] lg:text-[48px]">
+            {" "}
+            <span className="text-[#BE4E1E]"> Question</span>{" "}
+            <span style={{ color: "#842021" }}>Banks</span>
+          </h1>
+          <div className="flex flex-wrap items-center justify-center lg:items-stretch gap-10 mt-2 sm:mt-8 rounded-2xl shadow-lg ">
+            <div className="bg-[#F9F6EB] w-[400px] sm:w-[660px] h-[200px] md:h-[350px] lg:h-[500px] rounded-2xl flex items-center justify-center">
+              <Image
+                src="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/Homepage/Our+Platform+Features/Question+Banks/Question+bank+desktop.webp"
+                alt="question-bank"
+                className="mx-auto hidden lg:block px-0"
+                width={600}
+                height={400}
+                quality={100}
+              />
+               <Image
+                src="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/Homepage/Our+Platform+Features/Question+Banks/Question+bank+tablet.webp"
+                alt="question-bank"
+                className="mx-auto hidden md:block lg:hidden px-0"
+                width={500}
+                height={300}
+                quality={100}
+              />
+               <Image
+                src="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/Homepage/Our+Platform+Features/Question+Banks/Question+bank+mobile.webp"
+                alt="question-bank"
+                className="mx-auto block md:hidden lg:hidden px-0"
+                width={280}
+                height={200}
+                quality={100}
+              />
             </div>
-            <Footer />
+            <div className="flex flex-col justify-around px-5 lg:px-0">
+              <p className="text-[14px] sm:text-[20px] max-w-[520px] -mt-4 sm:mt-0 xl:-mb-12">
+                Our team has prepared thousands of FRM®/CFA/PRM practice
+                questions which would allow students to test their knowledge.
+              </p>
+              <div className="">
+                <p className="text-[#BE4E1E]  font-bold text-[16px] sm:text-[24px] mt-6 xl:-mt-8 mb-6 xl:-mb-14">
+                  <Link href="https://frm.midhafin.com/part-1/sample-questions">
+                  <span className="underline underline-offset-[4px]">
+                    View Sample Questions
+                  </span>{" "}
+                  <span>&#62;</span>{" "}
+                  </Link>
+                </p>
+                {/* <p className="text-[#BE4E1E] font-bold mt-8 text-[16px] sm:text-[20px] mb-6 xl:mb-0">
+                  <Link href="">
+                  <span className=" underline underline-offset-[4px]">
+                    FRM Part 2 Sample Questions
+                  </span>{" "}
+                  <span>&#62;</span>{" "}
+                  </Link>
+                </p> */}
+              </div>
+              <p className="text-[14px] sm:text-[20px] max-w-[520px] mb-8 xl:-mt-16">
+                <span className="font-bold">
+                  These questions are updated based on the current FRM®/CFA/PRM
+                  trends
+                </span>{" "}
+                and are a good mixture of easy, medium, and difficult questions.
+                An easy to comprehend solution for every single question is
+                provided along with the questions.
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-12 sm:mt-16 flex justify-center items-center mb-4">
+            <ul className="list-disc pl-5 text-[14px] sm:text-[20px]">
+              <li>
+                Our questions are mapped with the <span className="font-bold">FRM®/CFA/PRM exam&apos;s
+                latest curriculum.</span>
+              </li>
+              <li>
+                The organization structure of the FRM®/CFA/PRM questions is
+                <span className="font-bold">extremely user-friendly.</span>
+              </li>
+              <li>
+                The questions are <span className="font-bold">sequenced according to our student study plan.</span>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
-}
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
 export default MyComponents3;
