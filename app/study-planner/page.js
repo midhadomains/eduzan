3e8ddd5906d56bd88c8f05bbd488c27ade859201
midhadomains <159537481+midhadomains@users.noreply.github.com>
2024-06-17 12:12:03 +0530

    updated

--- app/study-planner/page.js
+++ app/study-planner/page.js
@@ -3,22 +3,22 @@ import Image from 'next/image'
 import Footer from '../../components/common/SiteFooter';
 import Navbar from '../../components/common/SiteHeader';
 
-export const metadata = {
-    title: "Student Reviews - MidhaFin | Hear from Our Successful Students",
-    description: "Discover what our students are saying about their experiences with MidhaFin. Read reviews and testimonials from successful FRM candidates who have benefited from our expert guidance and comprehensive resources.",
-    keywords: [
-      "MidhaFin",
-      "Student Reviews",
-      "Testimonials",
-      "FRM Exam Success",
-      "Student Feedback",
-      "Finance Education Reviews",
-      "FRM Certification Reviews",
-      "Course Reviews",
-      "Student Experiences",
-      "Success Stories"
-    ]
-  };
+// export const metadata = {
+//     title: "Student Reviews - MidhaFin | Hear from Our Successful Students",
+//     description: "Discover what our students are saying about their experiences with MidhaFin. Read reviews and testimonials from successful FRM candidates who have benefited from our expert guidance and comprehensive resources.",
+//     keywords: [
+//       "MidhaFin",
+//       "Student Reviews",
+//       "Testimonials",
+//       "FRM Exam Success",
+//       "Student Feedback",
+//       "Finance Education Reviews",
+//       "FRM Certification Reviews",
+//       "Course Reviews",
+//       "Student Experiences",
+//       "Success Stories"
+//     ]
+//   };
   
 const MyComponent = () => {
     return (
