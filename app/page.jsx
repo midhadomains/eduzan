15bab3945ceaad0f0810a24e61b789bf61fa2791
midhadomains <159537481+midhadomains@users.noreply.github.com>
2024-06-19 16:38:21 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ import Footer from "../components/common/SiteFooter";
 
 export const metadata = {
   title: "MidhaFin | Best FRM coaching in India",
-  description: "Choose GARP approved preparation provider,The Best FRM,CFA, and PRM coaching in India - MidhaFin,Boost your finance career with expert guidance and comprehensive study materials ",
+  description: "Choose GARP approved Best FRM coaching, The Best FRM,CFA, and PRM coaching in India - MidhaFin, Boost your finance career with expert guidance and comprehensive study materials ",
   keywords: [
     "best frm courses",
     " best frm course in india,best frm courses online",
