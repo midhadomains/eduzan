e6ded380ec599d2ee96c65d26fd0d40b23444d55
Sujal Sahu <sahusujal620@gmail.com>
2024-06-21 13:59:25 +0530

    done

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.midhafin.com', // Replace with your site URL
-    generateRobotsTxt: true, // (optional)
-    // Additional options can be added here
-};
