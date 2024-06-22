658d0b46c0b27a6a1704838a0b7afc26d3909e10
Sujal Sahu <sahusujal620@gmail.com>
2024-06-22 17:06:42 +0530

    dibe

--- app/reviews/page.js
+++ app/reviews/page.js
@@ -1,4 +1,10 @@
-import Reviews from "../../components/Reviews";
+import Image from "next/image";
+import Link from "next/link";
+import { FaLinkedin } from "react-icons/fa";
+import { IoStar, IoStarHalf, IoStarOutline } from "react-icons/io5";
+import fetchReviews from "../../lib/fetchReviews";
+import Navbar from "../../components/common/SiteHeader";
+import Footer from "../../components/common/SiteFooter";
 
 export const metadata = {
     title: "Student Reviews - MidhaFin | Hear from Our Successful Students",
@@ -17,16 +23,109 @@ export const metadata = {
     ]
 };
 
-export default function Page() {
+export default async function HomePage() {
+    function capitalizeWords(str) {
+        return str.replace(/\b\w/g, (match) => match.toUpperCase());
+    }
+
+    function formatName(name) {
+        const words = name.trim().split(/\s+/);
+        if (words.length >= 2) {
+            return `${words[0]} ${words[1]}`;
+        } else {
+            return `${words[0]}`;
+        }
+    }
+
+    const renderStars = (rating) => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating) {
+                stars.push(<IoStar key={i} className="text-[#f3c35d]" />);
+            } else if (i - rating <= 1) {
+                if (rating % 1 <= 0.2) {
+                    stars.push(<IoStarOutline key={i} className="text-[#f3c35d]" />);
+                } else if (rating % 1 > 0.2 && rating % 1 < 0.8) {
+                    stars.push(<IoStarHalf key={i} className="text-[#f3c35d]" />);
+                } else {
+                    stars.push(<IoStar key={i} className="text-[#f3c35d]" />);
+                }
+            } else {
+                stars.push(<IoStarOutline key={i} className="text-[#f3c35d]" />);
+            }
+        }
+        return stars;
+    };
+    let reviews = [];
+
+    try {
+        reviews = await fetchReviews();
+    } catch (error) {
+        console.error('Error fetching reviews:', error);
+    }
+
     return (
-        <>
-            <div className="select-none">
-                {/* <Reviews /> */}
-                <Reviews />
+        <div className="">
+            <Navbar/>
+            <div className="bg-[url('https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/Backgrounds/3500background.svg')] max-h-[300px] h-[100%] shadow-xl">
+                <div className="max-w-[1200px] px-[20px] mg:px-[50px] mx-auto py-4 mg:py-9">
+                    <h1 className="text-[30px] md:text-[40px] xl:text-[50px] my-2 mg:my-5 font-semibold text-[#6A1C1A]">
+                        Student <span className="text-[#BE4E1E]">Reviews</span>
+                    </h1>
+                    <p className="text-[16px] lg:text-[20px] max-w-[800px] lg:leading-[32px]">
+                        Discover what our students are saying about our transformative learning experiences.
+                    </p>
+                </div>
             </div>
-        </>
+
+            <div className="flex flex-wrap justify-center gap-8 my-[30px]">
+                {reviews.map((review, index) => (
+                    <div
+                        key={index}
+                        className="w-[330px] mg:w-[400px] aspect-square text-center  rounded-2xl shadow-[0_8px_30px_rgb(0,0,0,0.12)]   pt-4 mg:pt-8"
+                    >
+                        <div className="flex text-left items-center justify-center h-[120px] ">
+                            <Image
+                                width={120}
+                                height={120}
+                                src={review.image}
+                                alt={review.name}
+                                className="w-[100px] h-[100px] aspect-square flex-shrink-0 border-[#ffffff] rounded-[50%] border shadow-lg"
+                            />
+                            <div className="pl-[15px] mg:pl-[30px]">
+                                <h1 className="text-[18px] mg:text-[24px] font-[500]">
+                                    {capitalizeWords(formatName(review.name))}
+                                </h1>
+                                <h1 className="text-[14px] mg:text-[16px] font-[400] text-[#081A31] my-1 max-w-[210px]">
+                                    {review.job ? `Financial Risk Manager at  ${review.job}` : " "}{" "}
+                                </h1>
+                                <p>Passing year:&nbsp;{review.year}</p>
+                                <p className="text-[#BE4E1E] text-[16px] font-[400] flex text-center">
+                                    {capitalizeWords(review.country)}{" "}
+                                    <span className="text-slate-400">&nbsp;|&nbsp;</span>{" "}
+                                    <Link target="_blank" href={review.l_link}>
+                                        <FaLinkedin className="text-[25px] text-[#0072b1] hover:cursor-pointer" />
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+                        <p className="text-[#BE4E1E] text-[16px] mg:text-[18px] bg-[#F9F8F5] rounded-full my-5 w-fit mx-auto px-7 py-1 flex items-center">
+                            {capitalizeWords(review.course)}&nbsp; :&nbsp;&nbsp;
+                            <span className="text-[#f3c35d] flex pb-[3px] gap-1">
+                                {/* {Math.floor(review.rating) && review.rating > 0 ? Array(Math.floor(review.rating)).fill(<IoStar key={index} />) : null} */}
+                                {renderStars(review.rating)}
+                            </span>
+                        </p>
+                        <h1 className="text-center mg:text-left mx-auto text-[16px] mg:text-[18px] max-w-[350px] leading-[30px]">
+                            &quot; {`${review.excerpt}`}...
+                            <Link target='_blank' href={review.p_link} className="underline text-[#BE4E1E]">
+                                (Read More)&quot;
+                            </Link>{" "}
+                        </h1>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
-
-
-
+export const revalidate = 604800; // Revalidate every week (in seconds)
--- app/study-groups/page.jsx
+++ app/study-groups/page.jsx
@@ -1,9 +1,164 @@
-import React from 'react'
-
-export default function page() {
+import Image from "next/image";
+import Link from "next/link";
+import { IoIosArrowForward } from "react-icons/io";
+import Navbar from "../../components/common/SiteHeader";
+import Footer from "../../components/common/SiteFooter";
+export const metadata = {
+  title: "MidhaFin Study Groups | Connect with FRM Aspirants and Experts",
+  description: "Join the MidhaFin Study Group for collaborative learning with FRM aspirants and experts. Share resources, get study tips, and enhance your financial risk management knowledge. Connect with a community dedicated to your success.",
+  keyword: ["MidhaFin Study Group", "FRM study group", "Financial risk management study group", "FRM aspirants community", "FRM study resources",
+    " FRM study support", "FRM group learning", "Financial risk management study community", "FRM exam preparation group", "Study tips for FRM exam",
+    "Join MidhaFin's FRM study group",
+    "Connect with FRM aspirants at MidhaFin",
+    "Best study group for FRM exam preparation",
+    "Collaborative learning for financial risk management",
+    "How to join an FRM study group at MidhaFin",
+    "FRM certification study group",
+    "MidhaFin study community",
+    "FRM exam study help",
+    "Financial risk management exam support",
+    "FRM exam study partners"]
+};
+function page() {
   return (
     <div>
-      data
+      <Navbar />
+      <div className="bg-[url('https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/Backgrounds/3500background.svg')] md:py-[28px] py-[22px] lg:py-[43px]">
+        <h1 className="text-[#6A1C1A] md:text-[35px] sm:text-[24px] text-[20px] mx-auto lg:text-[50px] font-[600]  text-center   max-w-[1400px] px-[25px] md:px-[50px]">
+          MidhaFin <span className="text-[#BE4E1E]">Study Groups</span>
+        </h1>
+      </div>
+      <div className="md:px-[50px] px-[25px] mx-auto  max-w-[1400px] sm:mt-[27px] mt-[21px] lg:mt-[34px]">
+        <p className="leading-[23.1px] md:leading-[33px] lg:text-[20px] md:text-[16px] text-[14px]">
+          Dear Fellow FRM Candidates,
+          <br />
+          Are you preparing for the challenging Financial Risk Manager (FRM®)
+          exam &{" "}
+          <span className="font-[700]">
+            looking for a supportive community to share knowledge, insights and
+            motivation?
+          </span>{" "}
+          <br />
+          <span className="text-[#BE4E1E]">Look no further! </span>
+          <br />
+          We are excited to invite you to join dedicated FRM study groups on{" "}
+          <span className="font-[700]">Telegram, WhatsApp and Facebook.</span>
+        </p>
+      </div>
+      <div className="max-w-[1400px]  md:px-[50px] px-[25px] sm:mt-[40px] mt-[22px]  mx-auto flex justify-center mh:justify-between gap-[15px] mh:flex-nowrap flex-wrap ">
+        <div className="w-[50%] max-w-[617px] min-w-[300px] mh:mx-0 mx-auto ">
+          <Image
+            src="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/public/community.png"
+            width={900}
+            height={1000}
+            className="w-[100%] max-w-[617px] min-w-[300px]"
+            alt="Community"
+          />
+        </div>
+
+        <div className=" w-[50%] flex flex-col  gap-[13px] lx:gap-[48px] min-w-[310px]">
+          <h1 className="text-[12px] sm:text-[14px] lg:text-[18px] text-[#BE4E1E] leading-[27px] font-[700] tracking-wider mh:text-left text-center lx:mt-[77px]">
+            GET CONNECTED
+          </h1>
+          <div className="flex flex-col lg:gap-[53px] md:gap-[33px] sm:gap-[22px] gap-[43.11px]">
+            <div className="flex mh:flex-col gap-[17.13px] lg:gap-[23px] mh:ml-0 ml-[32px]">
+              <div className="flex items-center justify-normal gap-[14px]">
+                <Image
+                  src="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/public/community_wa.png"
+                  width={100}
+                  height={100}
+                  className="min-w-[45px] w-[50px]"
+                  alt="Community"
+                />
+                <span className="mh:block hidden lg:text-[18px] text-[14px]">
+                  Whatsapp Links
+                </span>{" "}
+              </div>
+              <div className="flex  lx:flex-row flex-col gap-[20px] lx:gap-[47.5px] xl:gap-[73px]">
+                <Link target="_blank" href="https://chat.whatsapp.com/HyO0yKKbdCmBkAW6VmoFfW" className="hover:text-[#BE4E1E] ">
+                  <u className="flex items-center gap-2 sm:text-[16px] text-[14px] xl:text-[20px]">
+                    FRM Part 1 Forum 2024 <IoIosArrowForward />
+                  </u>
+                </Link>
+                <Link target="_blank" href="https://chat.whatsapp.com/Fd88tCpmmbqJhuoIjk2qIZ" className="hover:text-[#BE4E1E]">
+                  <u className="flex items-center gap-2 hover:text-[#BE4E1] sm:text-[16px] text-[14px] xl:text-[20px]">
+                    FRM Part 2 Forum 2024 <IoIosArrowForward />
+                  </u>
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex mh:flex-col gap-[17.13px] lg:gap-[23px] mh:ml-0 ml-[32px]">
+              <div className="flex items-center justify-normal gap-[14px]">
+                <Image
+                  src="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/public/community_tg.png"
+                  width={100}
+                  height={100}
+                  className="min-w-[40px] w-[50px]"
+                  alt="community"
+                />
+                <span className="mh:block hidden lg:text-[18px] text-[14px]">
+                  Telegram Links
+                </span>{" "}
+              </div>
+              <div className="flex  lx:flex-row flex-col gap-[20px] lx:gap-[47.5px] xl:gap-[73px]">
+                <Link target="_blank" href="https://t.me/frm_1_forum" className="hover:text-[#BE4E1E] ">
+                  <u className="flex items-center gap-2 sm:text-[16px] text-[14px] xl:text-[20px]">
+                    FRM Part 1 Forum 2024 <IoIosArrowForward />
+                  </u>
+                </Link>
+                <Link target="_blank" href="https://t.me/frm_2_forum" className="hover:text-[#BE4E1E]">
+                  <u className="flex items-center gap-2 hover:text-[#BE4E1] sm:text-[16px] text-[14px] xl:text-[20px]">
+                    FRM Part 2 Forum 2024 <IoIosArrowForward />
+                  </u>
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex mh:flex-col gap-[17.13px] lg:gap-[23px] mh:ml-0 ml-[32px]">
+              <div className="flex items-center justify-normal gap-[14px]">
+                <Image
+                  src="https://midha-images.s3.ap-south-1.amazonaws.com/Midhafin/public/community_fb.png"
+                  width={100}
+                  height={100}
+                  className="min-w-[40px] w-[50px]"
+                  alt="community"
+                />
+                <span className="mh:block hidden lg:text-[18px] text-[14px]">
+                  Facebook Links
+                </span>{" "}
+              </div>
+              <div className="flex  lx:flex-row flex-col gap-[20px] lx:gap-[90.5px] xl:gap-[124px]">
+                <Link target="_blank" href="https://www.facebook.com/groups/FRMPart1" className="hover:text-[#BE4E1E] ">
+                  <u className="flex items-center gap-2 sm:text-[16px] text-[14px] xl:text-[20px]">
+                    FRM Part 1 Forum <IoIosArrowForward />
+                  </u>
+                </Link>
+                <Link target="_blank" href="https://www.facebook.com/groups/FRMPart2Forum" className="hover:text-[#BE4E1E]">
+                  <u className="flex items-center gap-2 sm:text-[16px] text-[14px] xl:text-[20px]">
+                    FRM Part 2 Forum <IoIosArrowForward />
+                  </u>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[46.5px] sm:mt-[64px] lx:mt-[111px] bg-[#EEEEEE]">
+        <div className="max-w-[1400px]  md:px-[50px] px-[25px] py-[22px] sm:py-[24px] mx-auto lx:py-[43px]">
+          <p className="text-[12px] mh:text-[14px] lx:text-[20px] ">
+            While these study groups offer numerous benefits, it&apos;s important to
+            manage group dynamics, maintain focus on the study objectives & use
+            these platforms in a way that complements your learning style and
+            preferences. Additionally, respecting the rules and guidelines set
+            by the group administrators can contribute to a positive and
+            productive learning experience.
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
+
+export default page;
